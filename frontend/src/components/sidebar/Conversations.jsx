4141ca61fc5d63e107772929f2d5c00a98ba9d77
Shubham Panwar <shubham043panwar@gmail.com>
2025-03-23 00:27:29 +0530

    2nd part completed frontend almost completed

---
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Conversation from "./Conversation";
+const Conversations = () => {
+  return (
+    <div>
+      <Conversation />
+      <Conversation />
+      <Conversation />
+    </div>
+  );
+};
+
+export default Conversations;
