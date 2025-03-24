3f08ce762d5212fdd7f664f0165b818e07f5c53d
Shubham Panwar <shubham043panwar@gmail.com>
2025-03-24 19:14:46 +0530

    completed project

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,67 +1,74 @@
 import Conversation from "../models/conversation.model.js";
 import Message from "../models/message.model.js";
-export const sendMessage = async(req,res)=>{
-    try {
-        const {message} = req.body;
-        const {id:receiverId} = req.params;
-        const senderId = req.user._id;
-        
-        let conversation = await Conversation.findOne({
-            participants:{$all:[senderId,receiverId]}
-        });
+import { getReceiverSocketId, io } from "../socket/socket.js";
 
-       if(!conversation){
-          conversation = await Conversation.create({
-              participants:[senderId,receiverId],
-          });
-       }
-      
-       const newMessage = new Message({
-            senderId,
-            receiverId,
-            message,
-       })
+export const sendMessage = async (req, res) => {
+  try {
+    const { message } = req.body;
+    const { id: receiverId } = req.params;
+    const senderId = req.user._id;
 
-       if(newMessage){
-          conversation.messages.push(newMessage._id);
-       }
-    
-       // this will run in series
-    //    await newMessage.save(); //1sec
-    //    await conversation.save();
- 
+    let conversation = await Conversation.findOne({
+      participants: { $all: [senderId, receiverId] },
+    });
 
-    //SOCKET IO FUNCTIONALITY
+    if (!conversation) {
+      conversation = await Conversation.create({
+        participants: [senderId, receiverId],
+      });
+    }
+
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      message,
+    });
+
+    if (newMessage) {
+      conversation.messages.push(newMessage._id);
+    }
+
+    // await conversation.save();
+    // await newMessage.save();
 
     // this will run in parallel
-    await Promise.all([newMessage.save(),conversation.save()]);
-       res.status(201).json(newMessage);
-    } catch (error) {
-        console.log("Error in sendMessage: ",error.message);
-        res.status(500).json({message:"Internal Server Error"});
+    await Promise.all([conversation.save(), newMessage.save()]);
+
+    // SOCKET IO FUNCTIONALITY WILL GO HERE
+    const receiverSocketId = getReceiverSocketId(receiverId);
+    if (receiverSocketId) {
+      // io.to(<socket_id>).emit() used to send events to specific client
+      io.to(receiverSocketId).emit("newMessage", newMessage);
     }
-}
-export const getMessages = async(req,res)=>{
-    try {
-        const {id:userToChatId} = req.params;
-        const senderId = req.user._id;
 
-        const conversation = await Conversation.findOne({
-            participants:{$all:[senderId,userToChatId]}
-        }).populate("messages");
+    res.status(201).json(newMessage);
+  } catch (error) {
+    console.log("Error in sendMessage controller: ", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
 
-        const messages = conversation.messages;
+export const getMessages = async (req, res) => {
+  try {
+    const { id: userToChatId } = req.params;
+    const senderId = req.user._id;
 
-        res.status(200).json(messages);
-    } catch (error) {
-        console.log("Error in getMessages: ", error.message);
-        res.status(500).json({ message: "Internal Server Error" });
-    }
-}
+    const conversation = await Conversation.findOne({
+      participants: { $all: [senderId, userToChatId] },
+    }).populate("messages"); // NOT REFERENCE BUT ACTUAL MESSAGES
 
+    if (!conversation) return res.status(200).json([]);
 
+    const messages = conversation.messages;
+
+    res.status(200).json(messages);
+  } catch (error) {
+    console.log("Error in getMessages controller: ", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
 
 //populate() method is used to populate the documents in the messages array of the conversation document.
-// The messages array contains the ids of the messages documents.   
+// The messages array contains the ids of the messages documents.
 // The populate() method will replace the ids with the actual message documents.
 // The messages array will now contain the actual message documents.
